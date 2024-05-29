b766745baf07eb15609ee320fc239f409609ae74
Mohammad Uvaiz <mohammeduvaiz0786@gmail.com>
2024-05-29 17:00:39 +0530

    Get request for needer forms

--- controllers/bloodNeederForm.js
+++ controllers/bloodNeederForm.js
@@ -7,3 +7,17 @@ module.exports.addFormData = async function  (req,res) {
         return res.status(200).send({message: 'Form SuccesFully Submitted'});
     }
 }
+
+module.exports.getAllNeederForms = async function (req, res) {
+    try {
+        const neederForms = await BloodNeederForm.find({});
+        if (neederForms) {
+            return res.status(200).send(neederForms);
+        } else {
+            return res.status(404).send({ message: 'No forms found' });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send({ message: 'An error occurred while retrieving the forms' });
+    }
+};
--- routes/form.js
+++ routes/form.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const {addFormData} = require('../controllers/bloodNeederForm');
+const {addFormData,getAllNeederForms} = require('../controllers/bloodNeederForm');
 const router = express.Router();
 router.post('/bloodNeederForms',addFormData)
+router.get('/allNeederForms',getAllNeederForms)
 
 module.exports = router
